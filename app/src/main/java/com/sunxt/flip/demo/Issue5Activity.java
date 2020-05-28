425045dd9149324fca61c26dabc7da15f9564e53
sunxintong <11083103@bbktel.com>
2020-05-28 11:20:57 +0800

    feat:初始化工程，https://github.com/openaphid/android-flip初始化android studio工程

---
+++ app/src/main/java/com/sunxt/flip/demo/Issue5Activity.java
@@ -0,0 +1,41 @@
+package com.sunxt.flip.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sunxt.flip.FlipViewController;
+import com.sunxt.flip.demo.issue5.GalleryFlipAdapter;
+import com.sunxt.flip.demo.R;
+
+public class Issue5Activity extends Activity {
+
+  private FlipViewController flipView;
+
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setTitle(R.string.activity_title);
+
+    flipView = new FlipViewController(this);
+
+    flipView.setAdapter(new GalleryFlipAdapter(this, flipView));
+
+    setContentView(flipView);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    flipView.onResume();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    flipView.onPause();
+  }
+}
